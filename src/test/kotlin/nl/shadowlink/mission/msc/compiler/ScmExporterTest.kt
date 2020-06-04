954ed7c2eb729f20b6bef8ad9bcc92e61706fc05
Kilian Steenman <37879201+KilianSteenman@users.noreply.github.com>
2020-06-04 08:30:27 +0200

    Add test to make sure calculated header size is equal to actual exported header size

--- src/test/kotlin/nl/shadowlink/mission/msc/compiler/ScmExporterTest.kt
+++ src/test/kotlin/nl/shadowlink/mission/msc/compiler/ScmExporterTest.kt
@@ -2,14 +2,34 @@ package nl.shadowlink.mission.msc.compiler
 
 import com.google.common.truth.Truth.assertThat
 import nl.shadowlink.mission.msc.fakes.FakeBinaryWriter
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 internal class ScmExporterTest {
 
     private val exporter = ScmExporter()
 
-    @Test
-    fun `header is exported`() {
+    @Nested
+    inner class HeaderTest {
+
+        @Test
+        fun `header size matches calculated header size`() {
+            val script = CompiledScript().apply {
+                addLine(LabelLine("Main"))
+                addLine(OpcodeLine("0001"))
+
+                addGlobal("PLAYER_CHAR")
+                addGlobal("PLAYER_ACTOR")
+            }
+
+            val bw = FakeBinaryWriter()
+            exporter.writeHeader(bw, script)
+
+            assertThat(bw.writtenBytes.size).isEqualTo(script.headerSize)
+        }
+
+        @Test
+        fun `header is exported`() {
 //        val script = CompiledScript().apply {
 //            addLine(LabelLine("Main"))
 //            addLine(OpcodeLine("0001"))
@@ -21,6 +41,7 @@ internal class ScmExporterTest {
 //        assertThat(bw.writtenBytes).containsExactly(
 //            listOf<Byte>(0x02, 0x0, 0x01)
 //        )
+        }
     }
 
     @Test
